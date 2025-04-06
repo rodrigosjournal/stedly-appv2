c051a235b32dec358362c674705c7fd649e7ef69
Rodrigo Flores <24555166+rodrigosjournal@users.noreply.github.com>
2025-04-06 19:51:24 +0200

    Create firebase.js

---
+++ firebase/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyARVe00XzKfXxjsla-nYyvwBxGjU4yFzcI",
+  authDomain: "steadly-9734e.firebaseapp.com",
+  projectId: "steadly-9734e",
+  storageBucket: "steadly-9734e.appspot.com",
+  messagingSenderId: "559712906722",
+  appId: "1:559712906722:web:4720b11c3008cafcfd9dbb",
+  measurementId: "G-ETMSVQQSES"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
