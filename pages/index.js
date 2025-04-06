b56a3f5e11f30e7a780a0598e95c70afd7e24456
Rodrigo Flores <24555166+rodrigosjournal@users.noreply.github.com>
2025-04-06 23:36:29 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,32 +2,25 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-black text-white font-sans flex flex-col items-center justify-between px-4 py-8 text-center">
-      {/* Brand + Slogan */}
-      <div className="mb-8">
-        <h1 className="text-3xl font-bold mb-2">steadly.app</h1>
-        <p className="text-3xl font-bold text-white">A minimalist tracker for your entire training journey</p>
-<p className="text-base text-white mt-2">Whether you're lifting, swimming, or in rehab — steadly.app helps you log consistently, reflect easily, and build insights over time.</p>
+    <div className="w-auto h-[50vh] rounded-2xl shadow-lg inline-block object-cover"
+          />
+          <img
+            src="/images/carousel/resting-athlete.jpg"
+            alt="Resting athlete"
+            className="h-[500px] rounded-2xl shadow-lg inline-block object-cover"
+          />
+          <img
+            src="/images/carousel/barbell-curl.jpg"
+            alt="Barbell curl"
+            className="h-[500px] rounded-2xl shadow-lg inline-block object-cover"
+          />
+          <img
+            src="/images/carousel/rock-climber.jpg"
+            alt="Rock climbing"
+            className="h-[500px] rounded-2xl shadow-lg inline-block object-cover"
+          />
+        </div>
       </div>
-
-      {/* CTA Buttons */}
-      <div className="flex gap-4 mb-10">
-        <Link href="/login">
-          <button className="bg-white text-black px-6 py-2 rounded-full shadow hover:bg-gray-200">Login</button>
-        </Link>
-        <Link href="/register">
-          <button className="border border-white px-6 py-2 rounded-full hover:bg-white hover:text-black">Register</button>
-        </Link>
-      </div>
-
-      {/* Hero Image */}
-      <img
-        src="/images/Hyrox.avif"
-        alt="Athlete training"
-        width={320}
-        height={480}
-        className="rounded-2xl shadow-lg mb-6"
-      />
     </div>
   );
 }
