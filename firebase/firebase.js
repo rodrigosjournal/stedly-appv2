a91da1e88b8aa8478aac9911da997123aa9745bd
Rodrigo Flores <24555166+rodrigosjournal@users.noreply.github.com>
2025-04-09 00:55:05 +0200

    Update firebase.js

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,5 +1,6 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyARVe00XzKfXxjsla-nYyvwBxGjU4yFzcI",
@@ -13,5 +14,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(app);
 
-export { db };
+export { auth, db };
